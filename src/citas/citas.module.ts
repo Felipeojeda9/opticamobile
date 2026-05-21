25c2a76f9fe6f73b8324f310e9b5bf0e511567f7
felipeojeda9 <felipeojeda64@gmail.com>
2026-05-21 18:00:40 -0400

    Se deja solo Mobile en raiz

--- src/citas/citas.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CitasService } from './citas.service';
-import { CitasController } from './citas.controller';
-
-@Module({
-  providers: [CitasService],
-  controllers: [CitasController]
-})
-export class CitasModule {}
